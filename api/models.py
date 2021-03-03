11249e35738d6723d638788701df1ebf7d379e2b
Alexis Craye <alexis1.craye@epitech.eu>
2021-03-03 18:28:11 +0100

    adding authentication

--- api/models.py
+++ api/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class MusicStyle(models.TextChoices):
+    ELECTRO = 'electro'
+
+
+class Playlist(models.Model):
+    title = models.TextField()
+    added_on = models.DateField(auto_now=True)
+
+
+class Album(models.Model):
+    title = models.TextField()
+    added_on = models.DateField(auto_now=True)
+    date = models.DateField()
+
+
+class Sound(models.Model):
+    title = models.TextField()
+    style = models.CharField(choices=MusicStyle.choices, max_length=0x100)
+    file = models.FileField()
+    added_on = models.DateField(auto_now=True, editable=False)
+    album = models.ForeignKey(Album, on_delete=models.CASCADE, related_name='sounds')
+    playlists = models.ManyToManyField(Playlist, related_name='sounds')
+    added_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sounds')
--- back/settings.py
+++ back/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api',
+    'oauth2_provider',
     'rest_framework',
 ]
 
@@ -117,16 +118,27 @@ USE_L10N = True
 
 USE_TZ = True
 
+# OAuth
+
+OAUTH2_PROVIDER = {
+    # this is the list of available scopes
+    'SCOPES': {'read': 'Read scope', 'write': 'Write scope', 'groups': 'Access to your groups'}
+}
+
 # Rest Framework
 
 REST_FRAMEWORK = {
     "DEFAULT_RENDERER_CLASSES": [
         'rest_framework.renderers.JSONRenderer',
     ],
-    # "DEFAULT_AUTHENTICATION_CLASSES": [],
-    # "DEFAULT_PERMISSION_CLASSES": [],
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
     "UNAUTHENTICATED_USER": None,
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': 100,
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'DEFAULT_FILTER_BACKENDS': [
         'rest_framework.filters.OrderingFilter'
