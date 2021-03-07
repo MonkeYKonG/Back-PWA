680602d20dbb0e42b242836270117750620baf48
Alexis Craye <alexis1.craye@epitech.eu>
2021-03-07 18:24:37 +0100

    Adding auto doc

--- back/urls.py
+++ back/urls.py
@@ -2,11 +2,24 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
 from django.contrib import admin
+from django.views.generic import TemplateView
+from rest_framework.schemas import get_schema_view
 
 admin.autodiscover()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
-    path('', include('api.urls'))
+    path('', include('api.urls')),
+    path('doc/', TemplateView.as_view(
+        template_name='redoc.html',
+        extra_context={'schema_url': 'openapi-schema'}
+    ), name='redoc'),
+    path('openapi/', get_schema_view(
+        title="PWA",
+        description="PWA project",
+        version="1.0.0",
+        authentication_classes=[],
+        permission_classes=[]
+    ), name='openapi-schema')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
