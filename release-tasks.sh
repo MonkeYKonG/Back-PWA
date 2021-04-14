1384b7ee48e07bc54a3c771c2103ab57520c165a
Alexis CRAYE <alexis.craye@rideyego.com>
2021-04-14 19:38:16 +0200

    fix release script

--- release-tasks.sh
+++ release-tasks.sh
@@ -1,3 +1,3 @@
 python manage.py migrate
 python manage.py loaddata style_fixture
-python manage.py createsuperuser --noinput
+python manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME
