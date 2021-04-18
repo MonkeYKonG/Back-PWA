56bfcdc4900f22368b265b1f5ca0f8fae0d0c018
Alexis Craye <alexis1.craye@epitech.eu>
2021-04-18 16:35:41 +0200

    remove trace from settings and be sure that deploy script will not crash if superuser already exists

--- release-tasks.sh
+++ release-tasks.sh
@@ -1,3 +1,3 @@
 python manage.py migrate
 python manage.py loaddata style_fixture
-python manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME
+python manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME || return 0
